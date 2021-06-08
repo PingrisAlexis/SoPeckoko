976b84497abf4932c565f91afc4cc2e5a9d357ae
Alexis <pingrisalexis@gmail.com>
2021-06-08 10:13:12 +0200

    add middlewares

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+//Import the package JsonWebToken 
+const jwt = require('jsonwebtoken');
+
+//Routes control: check the TOKEN of the user, if it matches the id of the user in the request, it will be allowed to change the corresponding data.
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+    const userId = decodedToken.userId;
+    if (req.body.userId && req.body.userId !== userId) {
+      throw 'Invalid user ID';
+    } else {
+      next();
+    }
+  } catch {
+    res.status(401).json({
+      error: new Error('Invalid request!')
+    });
+  }
+};
---
+++ middleware/multer-config.js
@@ -0,0 +1,23 @@
+//Import package MULTER, to manage images 
+const multer = require('multer');
+
+//List of accepted image formats
+const MIME_TYPES = {
+  'image/jpg': 'jpg',
+  'image/jpeg': 'jpg',
+  'image/png': 'png'
+};
+
+// Storage location and file name
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, 'images');
+  },
+  filename: (req, file, callback) => {
+    const name = file.originalname.split(' ').join('_');
+    const extension = MIME_TYPES[file.mimetype];
+    callback(null, name + Date.now() + '.' + extension);
+  }
+});
+
+module.exports = multer({ storage: storage }).single('image');
