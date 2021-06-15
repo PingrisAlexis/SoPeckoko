d04fb062d8d913bfb42d313515d07e53c16e62b1
Alexis <pingrisalexis@gmail.com>
2021-06-15 08:49:45 +0200

    Add passwordValidation

--- routes/user.js
+++ routes/user.js
@@ -5,10 +5,12 @@ const router = express.Router();
 
 //Import users controllers. Functions are associated with the different routes.
 const userCtrl = require('../controllers/user');
+//Import password rules. Functions are associated with the different routes.
+const passwordValidation = require('../middleware/password');
 
 //Route to create user.
 //Encrypts the user's password of the user, adds the user to the database.
-router.post('/signup', userCtrl.signup);
+router.post('/signup', passwordValidation, userCtrl.signup);
 //Route to login user.
 //Checks the identification identification information user's credentials, by returning the userID from the database and a signed signed JSON token.
 router.post('/login', userCtrl.login);
