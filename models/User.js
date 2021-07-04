640d4e3e6cfd4c90b4054e7dddd36753afee1e31
Alexis <pingrisalexis@gmail.com>
2021-07-04 11:59:30 +0200

    Deleting MaskData for mail

--- models/User.js
+++ models/User.js
@@ -7,7 +7,6 @@ const uniqueValidator = require('mongoose-unique-validator');
 //Define user's schema
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true, },
-  emailmasked: { type: String, required: true },
   password: { type: String, required: true },
 });
 
